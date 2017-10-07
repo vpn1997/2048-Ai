7369b9794406296e626c3ea1e2f7c93f6a9a4e35
Fredrik Engstrand <fredrik.engstrand@sheab.net>
2017-10-07 12:05:21 +0200

    Fixed unnecessary code-changes

--- puzzle.py
+++ puzzle.py
@@ -69,9 +69,9 @@ class GameGrid(Frame):
                 grid_row.append(t)
 
             self.grid_cells.append(grid_row)
-        scoreCell = Frame(background, bg=BACKGROUND_COLOR_CELL_EMPTY, width=SIZE, height=SIZE / GRID_LEN)
+        scoreCell = Frame(background, bg=BACKGROUND_COLOR_CELL_EMPTY, width=SIZE / GRID_LEN, height=SIZE / GRID_LEN)
         scoreCell.grid(row=0, column=2, columnspan=2, sticky=E+W, padx=GRID_PADDING, pady=GRID_PADDING)
-        scoreText = Label(master=scoreCell, text=str(self.totalScore), bg="#3C3738", fg="#BDC0BA",  justify=RIGHT, font=FONT, width=4, height=2)
+        scoreText = Label(master=scoreCell, text=str(self.totalScore), bg="#3C3738", fg="#BDC0BA",  justify=CENTER, font=FONT, width=4, height=2)
         #scoreText.grid()
         scoreText.pack(fill="x")
         self.grid_cells.append(scoreText)
