e56a84705a131c3e28ce08abd1957e7760be0719
Peter Wong <impeterwong@hotmail.com>
2017-10-19 23:44:09 +0800

    fix score calculation

--- logic.py
+++ logic.py
@@ -142,7 +142,7 @@ def merge(mat):
     for i in range(4):
          for j in range(3):
              if mat[i][j]==mat[i][j+1] and mat[i][j]!=0:
-                 score=mat[i][j]*2
+                 score += mat[i][j]*2
                  mat[i][j]*=2
                  mat[i][j+1]=0
                  done=True
